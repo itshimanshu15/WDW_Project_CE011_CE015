a6fd863039f7e112f740cd64fd180367157da783
itshimanshu15 <85964471+itshimanshu15@users.noreply.github.com>
2021-11-21 00:49:14 +0530

    codes

---
+++ quizjs/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following statements defines JavaScript correctly?",
+    answer: "It’s a scripting language used to make the website interactive",
+    options: [
+      "It’s an assembly language used to make the website interactive",
+      "It’s a scripting language used to make the website interactive",
+      "It’s compiled language used to make the website interactive",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: " JavaScript is a ____________ language.",
+    answer: "Object-Based",
+    options: [
+      "Assembly-language",
+      "Object-Oriented",
+      " High-level",
+      "Object-Based"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Arrays in JavaScript are defined by which of the following statements?",
+    answer: "It is an ordered list of values",
+    options: [
+      "It is an ordered list of values",
+      " It is an ordered list of string",
+      "It is an ordered list of functions",
+      "None"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Which of the following is not javascript data types?",
+    answer: "All of the mentioned",
+    options: [
+      "All of the mentioned",
+      "Number type",
+      "Undefined type",
+      "Null type"
+    ]
+  },
+  
+];
